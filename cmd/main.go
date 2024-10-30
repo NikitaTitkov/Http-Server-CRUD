9468d29d7258c3f6a1a0ae2880f9a33c1c464fab
TitkovNA <160733259+TitkovNA@users.noreply.github.com>
2024-10-30 18:43:24 +0300

    start

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"sync"
 
 	"github.com/fatih/color"
+	"github.com/go-chi/chi"
 )
+
+const (
+	baceurl           = "http://localhost:8080"
+	CreateUserPostfix = "/users"
+)
+
+type UserInfo struct {
+	Street string `json:"street"`
+	City   string `json:"city"`
+}
+
+type User struct {
+	Id    int64    `json:"id"`
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Email string   `json:"email"`
+	Info  UserInfo `json:"info"`
+}
+
+type syncMap struct {
+	elements map[int64]*User
+	mutex    sync.RWMutex
+}
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := &User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		http.Error(w, "Неверный формат запроса", http.StatusBadRequest)
+		return
+	}
+	err = json.NewEncoder(w).Encode(user)
+	if err != nil {
+		return
+	}
+	Users.mutex.RLock()
+	defer Users.mutex.RUnlock()
+
+}
+
+var Users = &syncMap{elements: make(map[int64]*User)}
 
 func main() {
 	fmt.Println(color.GreenString("Hello, world!"))
+	r := chi.NewRouter()
+	r.Post(CreateUserPostfix, CreateUserHandler)
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/IWorshipChaos
 
 go 1.23
 
-require github.com/fatih/color v1.17.0
+require (
+	github.com/fatih/color v1.17.0
+	github.com/go-chi/chi v1.5.5
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
