eb39058004deddda3a19613e13ceb605614fc333
titkovnikita <nikita.titckow@ya.ru>
2024-11-13 14:19:55 +0300

    some fixes

--- pkg/entities/users.go
+++ pkg/entities/users.go
@@ -17,8 +17,8 @@ type User struct {
 
 // UserCreateRequest represents the data for creating a user. Used for swagger docs.
 type UserCreateRequest struct {
-    Name  string   `json:"name"`
-    Age   int      `json:"age"`
-    Email string   `json:"email"`
-    Info  UserInfo `json:"info"`
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Email string   `json:"email"`
+	Info  UserInfo `json:"info"`
 }
